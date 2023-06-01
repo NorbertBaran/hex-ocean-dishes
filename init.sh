17645ba7bc61f836212837eb9fbef50fc447919a
Norbert Baran <norbertbaran1@gmail.com>
2023-06-01 11:55:53 +0200

    Clean init and launch scripts

--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
 sudo apt update
-sudo apt install curl git
 curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
 sudo apt install -y nodejs
+sudo apt install git -y
 git clone -b master https://github.com/NorbertBaran/hex-ocean-dishes.git
 cd hex-ocean-dishes
 npm install --global yarn
--- launch.sh
+++ launch.sh
@@ -1,6 +1,6 @@
 sudo apt update
-sudo apt install curl git -y
 curl -sSL https://get.docker.com/ | sudo sh
+sudo apt install git -y
 git clone -b master https://github.com/NorbertBaran/hex-ocean-dishes.git
 cd hex-ocean-dishes
 sudo docker build -t hex-ocean-dishes .
